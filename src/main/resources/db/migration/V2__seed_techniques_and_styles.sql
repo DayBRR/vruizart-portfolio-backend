42d35c924a442fbd237893b78eb28e56be034617
David Ruiz Requejo <davidrr.22.12@gmail.com>
2026-09-06 12:07:21 +0200

    feat: seed techniques and styles

---
+++ src/main/resources/db/migration/V2__seed_techniques_and_styles.sql
@@ -0,0 +1,17 @@
+INSERT INTO technique (name, description)
+VALUES
+    ('Óleo sobre lienzo', NULL),
+    ('Acrílico sobre lienzo', NULL),
+    ('Óleo sobre tabla', NULL),
+    ('Óleo sobre aluminio', NULL),
+    ('Pastel sobre papel', NULL),
+    ('Carboncillo sobre papel', NULL);
+
+INSERT INTO style (name, description)
+VALUES
+    ('Retrato', NULL),
+    ('Paisaje urbano', NULL),
+    ('Paisaje', NULL),
+    ('Objetos e interiores', NULL),
+    ('Temática social', NULL),
+    ('Otros motivos', NULL);
