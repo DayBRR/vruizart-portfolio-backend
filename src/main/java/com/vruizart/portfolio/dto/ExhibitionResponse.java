b9baf3707bfdcda6b33f09103ac1e5ad58411cf5
David Ruiz Requejo <davidrr.22.12@gmail.com>
2026-09-07 00:31:20 +0200

    feat: extend exhibition model with type and year

--- src/main/java/com/vruizart/portfolio/dto/ExhibitionResponse.java
+++ src/main/java/com/vruizart/portfolio/dto/ExhibitionResponse.java
@@ -1,13 +1,19 @@
 package com.vruizart.portfolio.dto;
 
+import com.vruizart.portfolio.entity.ExhibitionType;
+import lombok.Builder;
+
 import java.time.LocalDate;
 
+@Builder
 public record ExhibitionResponse(
         String title,
         String slug,
+        ExhibitionType type,
         String description,
         LocalDate startDate,
         LocalDate endDate,
+        Integer year,
         String locationName,
         String locationAddress,
         String imageUrl,
